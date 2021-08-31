eb69c5cae2415a581d4a28f23527cc3fb101087a
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-31 14:52:55 -0500

    completed exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        private string pinNumber = "1234";
+        private bool lockedAccount = false;
+       
+        public bool CheckPin(string userInput)
+        {
+            return (userInput == pinNumber);
+        }
+
+        public bool LogIntoAccount()
+        {
+            if (!lockedAccount)
+            {
+                int i = 0;
+                bool loggedIn = false;
+                while (i < 4)
+                {
+                    Console.WriteLine("Please enter your pin to login to your account.");
+                    string userInput = Console.ReadLine();
+                    if (CheckPin(userInput))
+                    {
+                        loggedIn = true;
+                        i = 4;
+
+                    }
+                    else if (i < 3)
+                    {
+                        i++;
+                    }
+                    if (i < 3)
+                    {
+                        Console.WriteLine("That was the incorrect pin number.\nPlease try again.");
+                    }
+                    if (i == 3)
+                    {
+                        Console.WriteLine("You have failed too many attempts.\n Your account has been locked");
+                        lockedAccount = true;
+                        i++;
+                    }
+                }
+                return loggedIn;
+            }
+            else
+            {
+                Console.WriteLine("Your account has had too many failed login attempts and has been locked.\n Please try again tomorrow.");
+                return false;
+            }
+        }
+
+        public void Deposit(double amountToDeposit)
+        {
+            
+            balance += amountToDeposit;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount();
+            string userInput = "";
+            Console.WriteLine("Welcome to YourAccount Banking");
+
+            if (myAccount.LogIntoAccount())
+            {
+                Console.WriteLine("\nYou have successfully logged into your account.\n");
+                do
+                {
+                    Console.WriteLine("What would you like to do?");
+                    Console.WriteLine("1. View Balance\n2. Make a deposit\n3. Exit");
+                    userInput = Console.ReadLine();
+                    if (userInput == "1")
+                    {
+                        Console.WriteLine($"\nYour current balance is ${myAccount.GetBalance()}.\n");
+                    }
+                    else if (userInput == "2")
+                    {
+                        Console.WriteLine("How much would you like to deposit?");
+                        myAccount.Deposit(Convert.ToDouble(Console.ReadLine()));
+                    }
+                } while (userInput != "3");
+            }
+        }
+    }
+}
